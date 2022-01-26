f91dd7a847551ccd814312bf87361139737722c2
peacedev89 <112556433+peacedev89@users.noreply.github.com>
2022-01-26 17:20:00 +0100

    d

--- packages/app/src/pages/ChatList/Linkman.tsx
+++ packages/app/src/pages/ChatList/Linkman.tsx
@@ -36,7 +36,12 @@ export default function Linkman({
         if (Time.isToday(nowTime, time)) {
             return Time.getHourMinute(time);
         }
-        
+        if (Time.isYesterday(nowTime, time)) {
+            return '昨天';
+        }
+        if (Time.isSameYear(nowTime, time)) {
+            return Time.getMonthDate(time);
+        }
         return Time.getYearMonthDate(time);
     }
 
